d001a378e9df7f750c7a1a74da4713fbcecde904
Reimyel <caiquegsambrana@gmail.com>
2026-03-06 02:00:33 -0300

    Exercícios 1 ao 6

---
+++ Estrutura de Decisão/Ex01.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa que peça dois números e imprima o maior deles.
+'''
+
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
