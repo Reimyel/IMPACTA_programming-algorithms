9bb5799d864352f1d566c25bdd1631718eb62877
Andressa <andressa.kitajima@aluno.faculdadeimpacta.com.br>
2026-03-10 20:41:40 -0300

    Ex 26 nn finalizado

--- Lista 01 - Estrutura de Decisão/Ex26.py
+++ Lista 01 - Estrutura de Decisão/Ex26.py
@@ -13,3 +13,30 @@ Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustív
 ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 e o
 preço do litro do álcool é R$ 1,90.
 '''
+print("A - Álcool\nG - Gasolina")
+combustivel = str(input("Qual combustível quer comprar? Informe apenas a inícial, como demonstrado acima: ")).lower()
+if combustivel == "a":
+    tipoCombustivel = "Álcool"
+elif combustivel == "g":
+    tipoCombustivel = "Gasolina"
+else:
+    print("Opção inválida!")
+    exit(0)
+
+litros = float(input("Quantos litros de combustível quer comprar?: "))
+if tipoCombustivel == "Álcool":
+    if litros <= 20:
+        desconto = (3 / 100) * litros
+    else:
+        desconto = (5 / 100) * litros
+    
+    precoAlc = (litros * 1.90) - desconto
+    print(precoAlc)
+else:
+    if litros <= 20:
+        desconto = (4 / 100) * litros
+    else:
+        desconto = (6 / 100) * litros
+    
+    precoGas = (litros * 2.50) - desconto
+    print(precoGas)
