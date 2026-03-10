fa21c8ef6a9bcc56f7271f9a9c7c6830e0e11b5b
Andressa <andressa.kitajima@aluno.faculdadeimpacta.com.br>
2026-03-10 19:16:29 -0300

    Ex 21

--- Lista 01 - Estrutura de Decisão/Ex21.py
+++ Lista 01 - Estrutura de Decisão/Ex21.py
@@ -11,4 +11,27 @@ notas de 100, uma nota de 50, quatro notas de 10, uma nota de 5 e quatro
 notas de 1.
 '''
 
-saque = input("Informe o valor do saque")
+print("BEM VINDO AO CAIXA ELETRÔNICO")
+saque = int(input("Informe um valor inteiro para saque de 10 a 600: "))
+
+if saque >= 10 and saque <= 600:
+    centena = int(saque / 100)
+    #REDUÇÃO DO SAQUE
+    saque = saque - (centena * 100)
+
+    cinquenta = int(saque / 50)
+    #REDUÇÃO DO SAQUE
+    saque = saque - (cinquenta * 50)
+
+    dezena = int(saque / 10)
+    #REDUÇÃO DO SAQUE
+    saque = saque - (dezena * 10)
+
+    cinco = int(saque / 5)
+    #REDUÇÃO DO SAQUE
+    saque = saque - (cinco * 5)
+
+    unidade = saque
+    print(str(centena) + " nota(s) de 100, " + str(cinquenta) + " nota(s) de 50, " + str(dezena) + " nota(s) de 10, " + str(cinco) + " nota(s) de 5 e " + str(unidade) + " nota(s) de 1.")
+else:
+    print("SAQUE NÃO AUTORIZADO. INFORME UMA QUANTIA INTEIRA ENTRE 10 E 600 REAIS.")
